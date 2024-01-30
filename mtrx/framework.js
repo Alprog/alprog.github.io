2ca2ea8a555d68f47db2a695355591723a281b27
Alexander Tuzhik <alprog@hotmail.com>
2024-01-30 22:41:29 +0100

    async=false

--- mtrx/framework.js
+++ mtrx/framework.js
@@ -2,6 +2,7 @@
 function loadScript(path)
 {
 	var script = document.createElement('script');
+	script.async = false;
 	script.src = path;
 	document.head.appendChild(script); 
 }
@@ -9,24 +10,23 @@ function loadScript(path)
 function loadStyle(path)
 {
 	var style = document.createElement('link');
+	style.async = false;
 	style.rel = "stylesheet";
 	style.href = path;
 	document.head.appendChild(style); 
 }
 
-document.body.onload = function()
-{
-	loadStyle("style.css")
+loadStyle("style.css")
 
-	loadScript("vector4.js")
-	loadScript("matrix4x4.js")
-	loadScript("math.js")
-	loadScript("canvas.js")
-	loadScript("pin.js")
-	loadScript("diagram.js")
+loadScript("vector4.js")
+loadScript("matrix4x4.js")
+loadScript("math.js")
+loadScript("canvas.js")
+loadScript("pin.js")
+loadScript("diagram.js")
 
-	const urlParams = new URLSearchParams(window.location.search);
-	const script = urlParams.get('script')
-	loadScript(script)
-}
+const urlParams = new URLSearchParams(window.location.search);
+const script = urlParams.get('script')
+loadScript(script)
+
 
