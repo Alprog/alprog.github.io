1aba227cfdd2d425406213eb188117fa9b5af43b
Alexander Tuzhik <alprog@hotmail.com>
2024-05-01 23:06:34 +0200

    Update model.js

--- mtrx/model.js
+++ mtrx/model.js
@@ -1,28 +1,37 @@
 
 var diagram = new Diagram();
 
+var rootObject = new Mesh();
+
 var model = new Airplane();
 model.transform = Matrix4x4.Scaling(Vector.One().get_scaled(10));
 
+var model = new Littleman();
+var S = Matrix4x4.Scaling(Vector.One().get_scaled(100));
+var R = Matrix4x4.RotationY_RHS(Math.PI / 4);
+model.transform = mult(R, S);
 
-model.children = [
-    new Line(Vector.ZeroPoint(), Vector.UnitX().get_scaled(10), "gray", 5),
-    new Line(Vector.ZeroPoint(), Vector.UnitY().get_scaled(10), "gray", 5),
-    new Line(Vector.ZeroPoint(), Vector.UnitZ().get_scaled(10), "gray", 5),
-    new Line(Vector.ZeroPoint(), Vector.UnitX().get_scaled(-10), "gray", 5),
-    new Line(Vector.ZeroPoint(), Vector.UnitY().get_scaled(-10), "gray", 5),
-    new Line(Vector.ZeroPoint(), Vector.UnitZ().get_scaled(-10), "gray", 5),
 
-    new Label("Left", Vector.UnitX().get_scaled(10)),
-    new Label("Up", Vector.UnitY().get_scaled(10)),
-    new Label("Backward", Vector.UnitZ().get_scaled(10)),
-    new Label("Right", Vector.UnitX().get_scaled(-10)),
-    new Label("Down", Vector.UnitY().get_scaled(-10)),
-    new Label("Forward", Vector.UnitZ().get_scaled(-10))
+rootObject.children = [
+    model,
+
+    new Line(Vector.ZeroPoint(), Vector.UnitX().get_scaled(100), "gray", 5),
+    new Line(Vector.ZeroPoint(), Vector.UnitY().get_scaled(100), "gray", 5),
+    new Line(Vector.ZeroPoint(), Vector.UnitZ().get_scaled(100), "gray", 5),
+    new Line(Vector.ZeroPoint(), Vector.UnitX().get_scaled(-100), "gray", 5),
+    new Line(Vector.ZeroPoint(), Vector.UnitY().get_scaled(-100), "gray", 5),
+    new Line(Vector.ZeroPoint(), Vector.UnitZ().get_scaled(-100), "gray", 5),
+
+    new Label("Left", Vector.UnitX().get_scaled(100)),
+    new Label("Up", Vector.UnitY().get_scaled(100)),
+    new Label("Backward", Vector.UnitZ().get_scaled(100)),
+    new Label("Right", Vector.UnitX().get_scaled(-100)),
+    new Label("Down", Vector.UnitY().get_scaled(-100)),
+    new Label("Forward", Vector.UnitZ().get_scaled(-100))
 ];
 
-diagram.addObject(model);
+diagram.addObject(rootObject);
 
-diagram.addObject(new ObjectRotator(model));
+diagram.addObject(new ObjectRotator(rootObject));
 
 dom_flush();
